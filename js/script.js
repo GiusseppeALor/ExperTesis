a9c77f96aa47004dd48a82e16fe998a6ea56b706
GiusseppeALor <112878090+GiusseppeALor@users.noreply.github.com>
2024-08-08 17:56:56 -0500

    avance 1.3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 document.getElementById('whatsappButton').addEventListener('click', function() {
-    window.location.href = 'https://wa.me/1234567890'; // Reemplaza con tu número de WhatsApp
+    window.location.href = 'https://wa.me/1234567890'; 
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const stepImage = document.getElementById('stepImage');
+
+    const imageMap = {
+        step1: "img/a.png",
+        step2: "img/a.jpg",
+        step3: "img/r.png",
+        step4: "img/t.png"
+    };
+
+    document.querySelectorAll('.step-box').forEach(box => {
+        box.addEventListener('click', function () {
+            const stepId = this.id;
+            const imgSrc = imageMap[stepId];
+            stepImage.src = imgSrc;
+        });
+    });
 });
